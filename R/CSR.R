46761dea49a68ecb17e417253d37d80fd6fcbd46
Soheil-A-Neshat <59550087+Soheil-A-Neshat@users.noreply.github.com>
2024-07-12 17:05:54 +0800

    Update CSR.R
    Bug fixes for assigning R category

--- R/CSR.R
+++ R/CSR.R
@@ -357,7 +357,7 @@ CSR_assign <- function(dAtA, var.name, p_adj, p.value.cutoff, Parallel, Vis) {
               dAtA_subset_SR <- dAtA_subset[which(dAtA_subset[,2] != Exp.Grp[1] & dAtA_subset[,3] == Exp.Grp[n.Exp.Grp]),]
               dAtA_subset_R <- rbind(dAtA_subset_CR, dAtA_subset_SR)
               dAtA_subset_R <- dAtA_subset_R[which(dAtA_subset_R[,11] == "*" & (dAtA_subset_R[,12] == "medium" | dAtA_subset_R[,12] == "large")),]
-              if(all(dAtA_subset_R[,4] > 0) & length(unique(dAtA_subset_R[,1]))>1) {
+              if(all(dAtA_subset_R[,4] > 0) & any(dAtA_subset_R[,2] == Exp.Grp[1]) & any(dAtA_subset_R[,3] == Exp.Grp[n.Exp.Grp])) {
                 a[i,2] <- "R"}
             }
       }else {
